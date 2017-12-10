fecbe325d0784b204d564cfe1bb7f0e5e1b1d5ea
leon.moore <leon.moore@USABOS208405L>
2017-12-10 13:15:41 -0600

    Added cors to get action method working and fixed ngRepeat:dupes

--- EmployeeManagementApp/EmployeeManagement.Client/MyScripts/EmployeeApp.js
+++ EmployeeManagementApp/EmployeeManagement.Client/MyScripts/EmployeeApp.js
@@ -1,6 +1,6 @@
 ﻿var app = angular.module("EmployeeApp", []);
 
-app.controller("EmployeeController", ['$scope', '$http', '$log', function myfunction($scope, $http, $log) {
+app.controller("EmployeeController", ['$scope', '$http', function myfunction($scope, $http) {
     var baseUrl = 'http://localhost:13108/api/Employee/';
 
     function getAllEmployees() {
@@ -16,7 +16,6 @@ app.controller("EmployeeController", ['$scope', '$http', '$log', function myfunc
             .then(function (response) {
                 $scope.employees = response.data;
                 $scope.Status = "Deleted Successfully";
-                getAllEmployees();
             });
         //alert(employee);
         //var index = $scope.employees.indexOf(employee);
